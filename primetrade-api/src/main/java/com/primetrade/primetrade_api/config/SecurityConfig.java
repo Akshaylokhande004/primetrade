aa64b431512a58d587b14abb96ab912d063895b9
mgg2 <mirlongoriya@gmail.com>
2026-02-25 15:21:51 +0530

    Properly enable CORS in SecurityFilterChain

--- primetrade-api/src/main/java/com/primetrade/primetrade_api/config/SecurityConfig.java
+++ primetrade-api/src/main/java/com/primetrade/primetrade_api/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .cors(cors -> {})
+                .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
